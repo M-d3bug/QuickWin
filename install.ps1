1ca443e5973c5d760d7a7bb4a74d6b08fb3236f5
M-d3bug <mehdim789+1@gmail.com>
2025-12-23 00:45:52 +0100

    script to run QuickWin using powershell

--- install.ps1
+++ install.ps1
@@ -1,1 +1,1 @@
-iwr https://raw.githubusercontent.com/M-d3bug/QuickWin/main/QuickWin.cmd -o $env:TEMP\QuickWin.cmd; saps $env:TEMP\QuickWin.bat -v RunAs
+iwr https://raw.githubusercontent.com/M-d3bug/QuickWin/main/QuickWin.cmd -o $env:TEMP\QuickWin.cmd; saps $env:TEMP\QuickWin.cmd -v RunAs
